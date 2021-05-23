37b851c1cb10ca02f8bccf250709d9259005ae26
Sujith Somraaj <sujithsomraaj@Sujiths-iMac.local>
2021-05-23 22:57:37 +0530

    setting up client

---
+++ src/dao/CreateMerchant.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./styles/CreateMerchant.css";
+
+export default class CreateMerchant extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Create Merchant</h1>
+        <p>
+          Every merchant in ZNFT should be approved by the ZNFT Share Holders.
+        </p>
+      </div>
+    );
+  }
+}
